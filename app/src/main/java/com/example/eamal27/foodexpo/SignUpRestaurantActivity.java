b0f8d587617d34d185f68179eb78061b71a86da3
eamal27 <elias.amal27@gmail.com>
2016-11-20 18:01:17 -0500

    Add signup views for user and restaurant

--- app/src/main/java/com/example/eamal27/foodexpo/SignUpRestaurantActivity.java
+++ app/src/main/java/com/example/eamal27/foodexpo/SignUpRestaurantActivity.java
@@ -2,6 +2,7 @@ package com.example.eamal27.foodexpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpRestaurantActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SignUpRestaurantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_restaurant);
+    }
+
+    public void submitRestaurantRegistration(View view) {
+
     }
 }
--- app/src/main/java/com/example/eamal27/foodexpo/SignUpUserActivity.java
+++ app/src/main/java/com/example/eamal27/foodexpo/SignUpUserActivity.java
@@ -2,6 +2,7 @@ package com.example.eamal27.foodexpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignUpUserActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SignUpUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_user);
+    }
+
+    public void submitUserRegistration(View view) {
+
     }
 }
