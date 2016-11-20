76d943be2c32c54cda76a41076eb9869a72d34ae
Thomas <tdfrantz@gmail.com>
2016-11-19 20:47:18 -0500

    Worked on userMain

--- app/src/main/java/com/example/eamal27/foodexpo/UserMainActivity.java
+++ app/src/main/java/com/example/eamal27/foodexpo/UserMainActivity.java
@@ -2,6 +2,7 @@ package com.example.eamal27.foodexpo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ToggleButton;
 
 public class UserMainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class UserMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
+    }
+
+    private void displayItem(FoodItem item){
+
     }
 }
