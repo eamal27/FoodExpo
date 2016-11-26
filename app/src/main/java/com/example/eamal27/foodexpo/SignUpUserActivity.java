c74b039ad57f908860dff3fb829418c679251fa5
Tdfrantz <tdfrantz@gmail.com>
2016-11-25 21:42:19 -0500

    Worked on the user sign up

--- app/src/main/java/com/example/eamal27/foodexpo/SignUpUserActivity.java
+++ app/src/main/java/com/example/eamal27/foodexpo/SignUpUserActivity.java
@@ -1,20 +1,67 @@
 package com.example.eamal27.foodexpo;
 
+import android.content.Intent;
+import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.Locale;
+
 public class SignUpUserActivity extends AppCompatActivity {
+
+    public static final int getTextAddress = 1111;
+    public static final int returnTextAddress = 1112;
+    public static final int cancelTextAddress = 1113;
+    public static final int getMapAddresss = 2222;
+    public static final int returnMapAddress = 2223;
+    public static final int cancelMapAddress = 2224;
+
+    private Address address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_user);
+        Locale locale = getResources().getConfiguration().locale;
+        address = new Address(locale);
     }
 
     public void submitUserRegistration(View view) {
 
     }
 
+    public void getTextAddress(View view){
+        Intent getAddress = new Intent(this, GetAddressText.class);
+        startActivityForResult(getAddress,getTextAddress);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent result){
+        super.onActivityResult(requestCode,resultCode,result);
+        if (requestCode==getTextAddress){
+            if (resultCode==returnTextAddress){
+                String addressOne = result.getStringExtra("addressOne");
+                String addressTwo = result.getStringExtra("addressTwo");
+                String city = result.getStringExtra("city");
+                String prov = result.getStringExtra("prov");
+                String country = result.getStringExtra("country");
+                String postal = result.getStringExtra("postal");
+
+                address.setAddressLine(0,addressOne);
+                address.setAddressLine(1,addressTwo);
+                address.setLocality(city);
+                address.setAdminArea(prov);
+                address.setCountryName(country);
+                address.setPostalCode(postal);
+
+                displayAddress();
+            }
+        }
+    }
+
+    private void displayAddress(){
+
+
+    }
     //Todo: at some point in the sign up user we should ask them for a delivery location which they can change later
 }
--- app/src/main/java/com/example/eamal27/foodexpo/User.java
+++ app/src/main/java/com/example/eamal27/foodexpo/User.java
@@ -1,8 +1,47 @@
 package com.example.eamal27.foodexpo;
 
+import android.location.Address;
+
 /**
  * Created by 100484424 on 11/21/2016.
  */
 
 public class User {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
